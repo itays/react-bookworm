3a38087b294f409e594ddca7b7e500457e0fd1a8
Itay Sidis <is874k@intl.att.com>
2017-12-24 12:51:55 +0200

    Redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,20 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { default as thunk } from 'redux-thunk';
+import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 ReactDOM.render(
   <BrowserRouter>
-  <App />
+  <Provider store={store}><App /></Provider>
   </BrowserRouter>,
   document.getElementById('root') as HTMLElement
 );
